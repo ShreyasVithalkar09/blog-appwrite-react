9b52536aba3c823c19fff9424a33d6e327b69639
ShreyasVithalkar09 <shreyas.vithalkar08@gmail.com>
2023-10-26 19:16:54 +0530

    fixed: card UI

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -6,10 +6,10 @@ import convertTime from "../utils/convertTime";
 function PostCard({ $id, title, featuredImage, $createdAt }) {
   return (
     <Link to={`/post/${$id}`}>
-      <div className="card-box w-80 max-h-80 h-56 rounded-md border border-gray-400 p-3 bg-[#FEFCF3] box-">
+      <div className="card-box w-80 max-h-80 h-56 rounded-md border border-gray-400 p-3 bg-[#FEFCF3] box-border flex flex-col items-center">
         <div className="image w-72 h-2/3 border rounded-md mb-2 box-border">
           <img
-            className="w-full h-full object-cover rounded-md box-border"
+            className="w-full h-full object-cover rounded-md box-border bg-white"
             src={appwriteService.getFilePreview(featuredImage)}
             alt={title}
           />
